3fc28d8c40b823dfdb5afc8ec249a087372514da
Himal Adhikari <himalad321@gmail.com>
2023-04-15 15:48:07 +0545

    added the config for sccache to work

--- third_script.ps1
+++ third_script.ps1
@@ -2,8 +2,10 @@ mkdir ~\Himal\AppData\Roaming\nushell;
 cp config_files\config.nu ~\AppData\Roaming\nushell\config.nu;
 cp config_files\config.nu ~\AppData\Roaming\nushell\env.nu;
 mkdir ~\AppData\Roaming\helix\;
-cp config_files\config.toml ~\AppData\Roaming\helix\config.toml;
+cp config_files\config.toml ~\AppData\Roaming\helix\hx_config.toml;
 mkdir ~\.config
 cp config_files\starship.toml ~\.config\starship.toml
 mkdir ~\AppData\Roaming\gitui\;
-cp config_files\theme.ron ~\AppData\Roaming\helix\theme.ron;
+cp config_files\theme.ron ~\AppData\Roaming\gitui\theme.ron;
+mkdir ~\.cargo
+cp config_files\sccache_config.toml ~\.cargo\config.toml
