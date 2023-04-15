d9dca935e462b17d72288b6442fe209ae5842de2
Himal Adhikari <himalad321@gmail.com>
2023-04-15 08:48:39 +0545

    back to using starship

--- third_script.ps1
+++ third_script.ps1
@@ -1,7 +1,7 @@
-mkdir ~\themes
-cp atomic.omp.json ~\themes\atomic.omp.json;
 mkdir ~\Himal\AppData\Roaming\nushell;
 cp config.nu ~\Himal\AppData\Roaming\nushell\config.nu;
 cp config.nu ~\Himal\AppData\Roaming\nushell\env.nu;
 mkdir ~\AppData\Roaming\helix\;
 cp config.toml ~\AppData\Roaming\helix\config.toml;
+mkdir ~\.config
+cp starship.toml ~\.config\starship.toml
