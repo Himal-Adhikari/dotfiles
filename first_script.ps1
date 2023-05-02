18bee242042a082e35445f31794e17ca87c81fda
Himal Adhikari <himalad321@gmail.com>
2023-05-02 19:59:08 +0545

    Linux

--- first_script.ps1
+++
@@ -1,7 +0,0 @@
-winget install KeePassXCTeam.KeePassXC;
-winget install VideoLAN.VLC;
-winget install qBittorrent.qBittorrent;
-winget install Microsoft.WindowsTerminal;
-winget install Notepad++.Notepad++;
-winget install Microsoft.VCRedist.2015+.x86;
-winget install SumatraPDF.SumatraPDF;
